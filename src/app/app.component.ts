a647ccee037fae8ae9d15cd3abbae47d0fbc1023
JoelC24 <libra-marriaga@hotmail.com>
2021-09-08 03:05:19 -0500

    Tarea terminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Error', url: '/folder/Error', icon: 'code' },
+    { title: 'Pagina', url: '/folder/Pagina', icon: 'code-slash' },
+    { title: 'Alterada', url: '/folder/Alterada', icon: 'cog' },
+    { title: 'por', url: '/folder/por', icon: 'color-filter' },
+    { title: 'su', url: '/folder/su', icon: 'color-wand' },
+    { title: 'bienestar', url: '/folder/bienestar', icon: 'diamond' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
+
+  public personalizados = ['Estos', 'Son', 'labels', 'personalizados', 'por', 'elSonYei']
 }
